975a6bd69209b69526e18735b693f4a5c4f5b4f6
luonancom <luonancom@qq.com>
2022-03-30 17:59:45 +0800

    log debug

--- util/log.go
+++ util/log.go
@@ -47,7 +47,7 @@ func initLogger() *logrus.Logger {
 	Log.Out = io.MultiWriter(os.Stdout, src)
 
 	//info level
-	Log.SetLevel(logrus.WarnLevel)
+	Log.SetLevel(logrus.InfoLevel)
 
 	Log.SetFormatter(&logrus.TextFormatter{
 		TimestampFormat: "2006-01-02 15:04:05",
