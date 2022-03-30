d1e76eede2f944203df03e9bf4a535cc9839f9d8
luonancom <luonancom@qq.com>
2022-03-30 14:59:07 +0800

    update DB_HOST env

--- util/config.go
+++ util/config.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
@@ -115,6 +116,25 @@ func InitConfig() {
 	}
 	Log.Errorln("环境变量MANAGER_DB_HOST：", os.Getenv("MANAGER_DB_HOST"))
 	Log.Errorln("最后采用了 ", cfg.Database.Host)
+
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, address := range addrs {
+
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				Log.Errorln("net.InterfaceAddrs：  ", ipnet.IP.String())
+
+			}
+
+		}
+	}
 
 	if os.Getenv("DB_NAME") != "" {
 		cfg.Database.DbName = os.Getenv("DB_NAME")
