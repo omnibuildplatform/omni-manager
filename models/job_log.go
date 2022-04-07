7ac3026d39637ad8bad2ad477af1e1dbee6081fa
luonancom <luonancom@qq.com>
2022-04-07 16:40:59 +0800

    mount rootfs volume

--- models/job_log.go
+++ models/job_log.go
@@ -178,8 +178,7 @@ func MakeJob(cm *v1.ConfigMap, buildtype, release string) (job *batchv1.Job, out
 		return
 	}
 	// cacheCurl := `curl -vvv -Ffile=@/opt/rootfs_cache/rootfs.tar.gz  -FfileType=image '` + util.GetConfig().K8sConfig.FfileType + `'`
-	omniImager := `omni-imager --package-list /conf/totalrpms.json --config-file /conf/conf.yaml --build-type ` + buildtype + ` --output-file ` + outputName
-	omniCurl := `curl -vvv -Ffile=@/opt/omni-workspace/` + outputName + ` -Fproject=` + release + `  -FfileType=image '` + util.GetConfig().K8sConfig.FfileType + `'`
+	omniImager := `omni-imager --package-list /conf/totalrpms.json --config-file /conf/conf.yaml --build-type ` + buildtype + ` --output-file ` + outputName + ` && curl -vvv -Ffile=@/opt/omni-workspace/` + outputName + ` -Fproject=` + release + `  -FfileType=image '` + util.GetConfig().K8sConfig.FfileType + `'`
 	jobInterface := clientset.BatchV1().Jobs(util.GetConfig().K8sConfig.Namespace)
 	var backOffLimit int32 = 0
 	var tTLSecondsAfterFinished int32 = 1800
@@ -220,7 +219,7 @@ func MakeJob(cm *v1.ConfigMap, buildtype, release string) (job *batchv1.Job, out
 								"-c",
 								// cacheCurl,
 								omniImager,
-								omniCurl,
+								// omniCurl,
 							},
 							VolumeMounts: []v1.VolumeMount{
 								{
